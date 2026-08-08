c99c8f15fba3fcf6df57b256ae1f172dde87c54a
Hera Silveira Ramos <hera.ramos01@gmail.com>
2026-08-07 22:41:18 -0300

    Iniciando aula web 07/08

---
+++ src/telas/cad-usuario.js
@@ -0,0 +1,10 @@
+function CadUsuario() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Cadastro de Usuário</Text>
+    </View>
+  );
+}
+
+
+export default CadUsuario;
